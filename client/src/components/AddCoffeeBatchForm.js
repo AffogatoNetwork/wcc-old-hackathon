c0a0b0d1a79542a2b4ab675f70b4826f70d7c928
= <=>
2019-07-07 15:16:31 -0600

    Handling forms with formik

--- client/src/components/AddCoffeeBatchForm.js
+++ client/src/components/AddCoffeeBatchForm.js
@@ -1,35 +1,61 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 
-class AddCoffeeBatchForm extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {tokenAmt: ''};
-    
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-      }
-    
-      handleChange(event) {
-        this.setState({tokenAmt: event.target.value});
-      }
-    
-      handleSubmit(event) {
-        alert('Minting new tokens: ' + this.state.tokenAmt);
-        this.props.onTokenMint(this.state.tokenAmt)
-        event.preventDefault();
-      }
-    
-      render() {
-        return (
-          <form onSubmit={this.handleSubmit}>
-            <label>
-              Amount of tokens: 
-              <input type="text" value={this.state.tokenAmt} onChange={this.handleChange} />
-            </label>
-            <input type="submit" value="Submit" />
-          </form>
-        );
-      }
+function Inputform(props) {
+  const submitFn =  props.onSubmitFn;
+  return (
+    <div>
+      <h1>Add coffee batch:</h1>
+      <Formik
+        initialValues={{
+          producer: "",
+          address: "",
+          amount: 0,
+          description: ""
+        }}
+        validate={values => {
+          let errors = {};
+          if (!values.address) {
+            errors.address = "Required";
+          }
+          if (!values.description) {
+            errors.description = "Required";
+          }
+          if (!values.amount || values.amount < 0) {
+            errors.amunt = "Must be a positive value";
+          }
+          return errors;
+        }}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 400);
+          submitFn(values);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <Field type="text" name="producer" placeholder=" Producer Name" />
+            <ErrorMessage name="producer" component="div"  />
+            <Field type="text" name="address" placeholder="Producer Address" />
+            <ErrorMessage name="address" component="div" />
+            <Field type="text" name="amount" placeholder="Amount" />
+            <ErrorMessage name="amount" component="div" />
+            <Field type="text" name="description" placeholder="Coffee Description" />
+            <ErrorMessage name="description" component="div" />
+            <button type="submit" disabled={isSubmitting}>
+              Submit
+            </button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+}
+
+function AddCoffeeBatchForm(props) {
+  return <Inputform onSubmitFn={props.onCoffeeBatchAdd} />;
 }
 
 export default AddCoffeeBatchForm;
