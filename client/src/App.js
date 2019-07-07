b1ce90d037c1b55de5c1b2cd27fe7e38346bf1b7
= <=>
2019-07-07 12:26:21 -0600

    Adding mint tokens form

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 import SimpleStorageContract from "./contracts/SimpleStorage.json";
 import getWeb3 from "./utils/getWeb3";
-
+import ContractForm from "./components/ContractForm"
 import "./App.css";
 
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+  state = { allTokens: 0, storageValue: 0, web3: null, accounts: null, contract: null };
 
   componentDidMount = async () => {
     try {
@@ -25,7 +25,7 @@ class App extends Component {
 
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
+      this.setState({ web3, accounts, contract: instance });
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
@@ -48,23 +48,22 @@ class App extends Component {
     this.setState({ storageValue: response });
   };
 
+  mintTokens = async (tokenAmt) => {
+    const {contract} = this.state;
+    await contract.methods.mintTokens(tokenAmt);
+    const response = await contract.methods.mintTokens().call();
+    this.setState({ allTokens: tokenAmt });
+  }
+
   render() {
     if (!this.state.web3) {
       return <div>Loading Web3, accounts, and contract...</div>;
     }
     return (
       <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 40</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
+        <h1>Wrapped Coffee coins</h1>
+        <ContractForm onTokenMint = {this.mintTokens}/>
+        <button onClick={()=>this.runExample()}>run example?</button>
       </div>
     );
   }
