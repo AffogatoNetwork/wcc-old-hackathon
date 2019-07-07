cc97be45e60fa258facfd0d1519647bae977c809
Cristian Espinoza <ceegarner@gmail.com>
2019-07-07 15:19:12 -0600

    updated contracts

--- contracts/WrappedCoffeeCoin.sol
+++ contracts/WrappedCoffeeCoin.sol
@@ -8,6 +8,11 @@ contract WrappedCoffeeCoin is ERC20Mintable, ERC20Detailed, Ownable {
 
     mapping(address => bool) public isCooperative;
 
+    modifier onlyCooperative(){
+        require(isCooperative[msg.sender], "user must be a cooperative");
+        _;
+    }
+
     constructor() Ownable() ERC20Detailed("Wrapped Coffee", "WCC", 0) public {
         // _mint(msg.sender);
     }
@@ -18,5 +23,13 @@ contract WrappedCoffeeCoin is ERC20Mintable, ERC20Detailed, Ownable {
 
     function removeCooperative(address cooperative) public onlyOwner {
         isCooperative[cooperative] = false;
+    }
+
+    function wrapCoffee(address _owner, uint _amount) public onlyCooperative {
+        _mint(_owner, _amount);
+    }
+
+    function unwrapCoffee(address _owner, uint _amount) public onlyCooperative {
+         _burn(_owner, _amount);
     }
 }
