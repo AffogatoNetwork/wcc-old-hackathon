167ad0e2a15af00e871c9115372e2b5ca6e12bb6
Cristian Espinoza <ceegarner@gmail.com>
2019-07-07 16:24:31 -0600

    get tokens by owner

--- contracts/CoffeeBatchNFT.sol
+++ contracts/CoffeeBatchNFT.sol
@@ -31,4 +31,29 @@ contract CoffeeBatchNFT is ERC721Full, ERC721Mintable{
         _setTokenURI(coffeeId.current(), _tokenURI);
         return true;
     }
+
+     function tokensOfOwner(address _owner) external view returns(uint256[] memory ownerTokens) {
+        uint256 tokenCount = balanceOf(_owner);
+
+        if (tokenCount == 0) {
+            // Return an empty array
+            return new uint256[](0);
+        } else {
+            uint256[] memory result = new uint256[](tokenCount);
+            uint256 totalCoffee = totalSupply();
+            uint256 resultIndex = 0;
+
+            // We count on the fact that all Swag have IDs starting at 1 and increasing
+            // sequentially up to the totalCat count.
+            uint256 coffeeIterator;
+
+            for (coffeeIterator = 1; coffeeIterator <= totalCoffee; coffeeIterator++) {
+                if (ownerOf(coffeeIterator) == _owner) {
+                    result[resultIndex] = coffeeIterator;
+                    resultIndex++;
+                }
+            }
+            return result;
+        }
+    }
 }
